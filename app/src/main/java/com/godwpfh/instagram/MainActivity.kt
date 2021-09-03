87b0ec1253b3ca153a1e836a967147b4689e4340
godwpfh <0112jyoung@naver.com>
2021-09-03 17:26:06 +0900

    Add navigationBar on MainActivity.kt

--- app/src/main/java/com/godwpfh/instagram/MainActivity.kt
+++ app/src/main/java/com/godwpfh/instagram/MainActivity.kt
@@ -1,13 +1,53 @@
 package com.godwpfh.instagram
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.godwpfh.instagram.navigation.AlarmFragment
+import com.godwpfh.instagram.navigation.DetailViewFragment
+import com.godwpfh.instagram.navigation.GridFragment
+import com.godwpfh.instagram.navigation.UserFragment
+import com.google.android.material.navigation.NavigationBarView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),  NavigationBarView.OnItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //라라라랄
+        bottom_navigation.setOnItemSelectedListener(this)
     }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_home -> {
+                var detailViewFragment=DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridFragment=GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment=AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
+                return true
+            }
+            R.id.action_account -> {
+                var userFragment=UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
+
+
 }
+
+
